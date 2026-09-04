7967d5fe6cb6120b6087cfe121dccd02e81baaf9
SnehaSharma00 <sanuwkuku@gmail.com>
2026-09-04 22:25:21 +0530

    permutations of a number

---
+++ Recursion/permutations.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void rec(int n, string &s, vector<string>&ans, int idx){
+if(idx == n){
+    return;
+}
+
+    for(int i=idx; i<n; i++){
+        ans.push_back(s);
+        swap(s[i], s[idx]);
+        rec(n, s, ans, idx+1);
+        swap(s[i], s[idx]);
+    }
+    
+}
+
+
+int main(){
+
+    int n;
+    cin>>n;
+    
+    int a;
+    cin>>a;
+
+    string s = to_string(a);
+
+    vector<string>ans;
+
+    rec(n, s, ans, 0);
+    return 0;
+}
