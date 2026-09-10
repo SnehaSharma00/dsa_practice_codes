e561200f40b7674278e21bab80009d65e687fdec
SnehaSharma00 <sanuwkuku@gmail.com>
2026-09-10 23:56:25 +0530

    vacation description

---
+++ DP/vacation_atcoder.cpp
@@ -0,0 +1,34 @@
+//2d dp question
+/*C - Vacation  / 
+Time Limit: 2 sec / Memory Limit: 1024 MiB
+
+Score : 
+100 points
+
+Problem Statement
+Taro's summer vacation starts tomorrow, and he has decided to make plans for it now.
+
+The vacation consists of 
+N days. For each 
+i (
+1≤i≤N), Taro will choose one of the following activities and do it on the 
+i-th day:
+
+A: Swim in the sea. Gain 
+a 
+i
+​
+  points of happiness.
+B: Catch bugs in the mountains. Gain 
+b 
+i
+​
+  points of happiness.
+C: Do homework at home. Gain 
+c 
+i
+​
+  points of happiness.
+As Taro gets bored easily, he cannot do the same activities for two or more consecutive days.
+
+Find the maximum possible total points of happiness that Taro gains.*/
