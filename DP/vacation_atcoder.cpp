364fed32f806b304d1d5966147400d95c080a9dc
SnehaSharma00 <sanuwkuku@gmail.com>
2026-09-10 23:57:09 +0530

    vacation description

--- DP/vacation_atcoder.cpp
+++ DP/vacation_atcoder.cpp
@@ -31,4 +31,100 @@ i
   points of happiness.
 As Taro gets bored easily, he cannot do the same activities for two or more consecutive days.
 
-Find the maximum possible total points of happiness that Taro gains.*/
+Find the maximum possible total points of happiness that Taro gains.
+
+Constraints
+All values in input are integers.
+1≤N≤10 
+5
+ 
+1≤a 
+i
+​
+ ,b 
+i
+​
+ ,c 
+i
+​
+ ≤10 
+4
+ 
+Input
+Input is given from Standard Input in the following format:
+
+N
+a 
+1
+​
+  
+b 
+1
+​
+  
+c 
+1
+​
+ 
+a 
+2
+​
+  
+b 
+2
+​
+  
+c 
+2
+​
+ 
+:
+a 
+N
+​
+  
+b 
+N
+​
+  
+c 
+N
+​
+ 
+Output
+Print the maximum possible total points of happiness that Taro gains.
+
+Sample Input 1
+Copy
+3
+10 40 70
+20 50 80
+30 60 90
+Sample Output 1
+Copy
+210
+If Taro does activities in the order C, B, C, he will gain 
+70+50+90=210 points of happiness.
+
+Sample Input 2
+Copy
+1
+100 10 1
+Sample Output 2
+Copy
+100
+Sample Input 3
+Copy
+7
+6 7 8
+8 8 3
+2 5 2
+7 8 6
+4 6 8
+2 3 4
+7 5 1
+Sample Output 3
+Copy
+46
+Taro should do activities in the order C, A, B, A, C, B, A.*/
+
