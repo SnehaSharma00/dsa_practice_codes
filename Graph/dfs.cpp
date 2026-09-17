a42a8ddd9d7a0ea206bb5e87f3091ea15d054614
SnehaSharma00 <sanuwkuku@gmail.com>
2026-09-17 23:46:22 +0530

    .

--- Graph/dfs.cpp
+++ Graph/dfs.cpp
@@ -78,17 +78,17 @@ void dfs(int i, vector<int>& adj[], vector<int>& vis){
     }
 }
 
-int main(){
-    int n, m;
-    cin>>n>>m;
-    vector<int> adj[n+1];
+// int main(){
+//     int n, m;
+//     cin>>n>>m;
+//     vector<int> adj[n+1];
 
-    for(int i=0; i<m; i++){
-        int u, v;
-        cin>>u>>v;
-        adj[u].push_back(v);
-        adj[v].push_back(u);
-    }
+//     for(int i=0; i<m; i++){
+//         int u, v;
+//         cin>>u>>v;
+//         adj[u].push_back(v);
+//         adj[v].push_back(u);
+//     }
 
 //     vector<int>vis(n+1, 0);
 
